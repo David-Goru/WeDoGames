2b5a6780f337e84bddee67abb4bccc912b669a8e
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2020-10-21 17:10:09 +0200

    Added AI scene (again)

---
+++ Tower Defense/Assets/Scripts/AI/Base_AI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Base_AI : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    public Transform Goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(Goal.position);
+    }
+}
