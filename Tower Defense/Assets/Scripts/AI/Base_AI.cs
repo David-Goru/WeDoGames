173ae1d06a54f431972bd732c6204ddfc773a2c5
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2020-10-21 17:45:33 +0200

    Finite state machine created

--- Tower Defense/Assets/Scripts/AI/Base_AI.cs
+++ Tower Defense/Assets/Scripts/AI/Base_AI.cs
@@ -7,17 +7,22 @@ public class Base_AI : MonoBehaviour
 {
 
     private NavMeshAgent agent;
+    private Animator anim;
+    private State currentState;
+
     public Transform Goal;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        currentState = new Move(this.gameObject, anim, Goal, agent);
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(Goal.position);
+        currentState = currentState.Process();
     }
 }
