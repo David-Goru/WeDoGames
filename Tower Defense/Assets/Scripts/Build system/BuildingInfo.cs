047d9a0ae3642b4c862e6f87c24e2b47244502f1
arturo101199 <al375559@uji.es>
2020-10-28 17:33:17 +0100

    Made BuildingInfo

---
+++ Tower Defense/Assets/Scripts/Build system/BuildingInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Building", menuName = "ScriptableObjects/BuildingInfo", order = 0)]
+public class BuildingInfo : ScriptableObject
+{
+    [SerializeField] protected float hp;
+    [SerializeField] protected float price;
+    [SerializeField] protected Pool turretPool;
+
+    public float GetHp() { return hp; }
+    public float GetPrice() { return price; }
+    public Pool GetTurretPool() { return turretPool; }
+}
--- Tower Defense/Assets/Scripts/Turrets/TurretInfo.cs
+++ Tower Defense/Assets/Scripts/Turrets/TurretInfo.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Turret", menuName = "ScriptableObjects/TurretInfo", order = 0)]
-public class TurretInfo : ScriptableObject
+public class TurretInfo : BuildingInfo
 {
-    [SerializeField] float hp;
+
     [SerializeField] float attackRate;
     [SerializeField] float attackDamage;
     [SerializeField] float attackRange;
-    [SerializeField] float price;
-    [SerializeField] Pool turretPool;
 
-    public float GetHp() { return hp; }
     public float GetAttackRate() { return attackRate; }
     public float GetAttackDamage() { return attackDamage; }
     public float GetAttackRange() { return attackRange; }
-    public float GetPrice() { return price; }
-    public Pool GetTurretPool() { return turretPool; }
+
 }
