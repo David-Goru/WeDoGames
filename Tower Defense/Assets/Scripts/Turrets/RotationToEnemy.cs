4016761de7cb7d5226b6c58dad0c16456a73c3bd
arturo101199 <al375559@uji.es>
2020-11-10 22:08:24 +0100

    updated cannon prefab to implement turret behaviour

--- Tower Defense/Assets/Scripts/Turrets/RotationToEnemy.cs
+++ Tower Defense/Assets/Scripts/Turrets/RotationToEnemy.cs
@@ -6,6 +6,9 @@ public class RotationToEnemy : MonoBehaviour
 {
     public void RotateToEnemy(Transform enemy)
     {
+        float xRot = transform.rotation.eulerAngles.x; 
+        float zRot = transform.rotation.eulerAngles.z;
         this.transform.LookAt(enemy);
+        transform.rotation = Quaternion.Euler(xRot, transform.rotation.eulerAngles.y, zRot);
     }
 }
