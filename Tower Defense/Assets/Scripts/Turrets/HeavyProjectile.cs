982ec2df7d9ea721dfe00c25083035604eb0c217
arturo101199 <al375559@uji.es>
2020-11-10 20:15:44 +0100

    Cleaned the code of heavy projectile

--- Tower Defense/Assets/Scripts/Turrets/HeavyProjectile.cs
+++ Tower Defense/Assets/Scripts/Turrets/HeavyProjectile.cs
@@ -7,7 +7,6 @@ public class HeavyProjectile : Projectile
     Vector3 lastEnemyPosition;
     const float g = 9.8f;
 
-    [SerializeField] float timeToReachTarget;
     float vz;
     float vy;
     float y0;
@@ -25,22 +24,40 @@ public class HeavyProjectile : Projectile
 
     void initializeParameters()
     {
+        //reset parameters
         time = 0;
         lastZ = 0;
         lastY = transform.position.y;
+
+        //Put the y at the same height of the target to get horizontal distance
         transform.position = new Vector3(transform.position.x, lastEnemyPosition.y, transform.position.z);
+
+        //Calculate horizontal distance
         float z = Vector3.Distance(transform.position, lastEnemyPosition);
+
+        //Reset y to his previous value
         transform.position = new Vector3(transform.position.x, lastY, transform.position.z);
+
+        //Calculate vertical distance
         y0 = transform.position.y;
         float h = y0 - lastEnemyPosition.y;
+
+        //Check if the speed is enough to reach target
         float velocity = speed;
-        while (float.IsNaN(Mathf.Acos(((g * z * z) / (velocity * velocity) - h) / Mathf.Sqrt(h * h + z * z))))
+        float sqrt = Mathf.Sqrt(h * h + z * z);
+        while (float.IsNaN(Mathf.Acos(((g * z * z) / (velocity * velocity) - h) / sqrt)))
         {
             velocity++;
         }
-        angle = (Mathf.Acos(((g * z * z) / (velocity * velocity) - h) / Mathf.Sqrt(h * h + z * z)) + Mathf.Atan2(z, h)) / 2;
+
+        //calculate angle
+        angle = (Mathf.Acos(((g * z * z) / (velocity * velocity) - h) / sqrt) + Mathf.Atan2(z, h)) / 2;
+
+        //calculate velocities
         vz = velocity * Mathf.Cos(angle);
         vy = velocity * Mathf.Sin(angle);
+
+        //Orientate projectile to target
         transform.LookAt(target);
         transform.localRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
     }
@@ -48,9 +65,20 @@ public class HeavyProjectile : Projectile
     protected override void updateProjectile()
     {
         time += Time.deltaTime;
+
+        calculateHorizontalPosition();
+        calculateVerticalPosition();
+    }
+
+    void calculateHorizontalPosition()
+    {
         float z = vz * time;
         transform.Translate(Vector3.forward * (z - lastZ), Space.Self);
         lastZ = z;
+    }
+
+    void calculateVerticalPosition()
+    {
         float y = y0 + (vy * time) - (g / 2) * (time * time);
         transform.Translate(Vector3.up * (y - lastY), Space.World);
         lastY = y;
