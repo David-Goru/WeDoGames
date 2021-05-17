7d44b63af0748646e70a75c075eba8d06adb3410
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-05-17 13:26:24 +0200

    ChatLog commands support added

--- Tower Defense/Assets/Scripts/GameManager.cs
+++ Tower Defense/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,35 +11,70 @@ public class GameManager : MonoBehaviour
 
     public int maxMessages = 25;
 
-    public GameObject chatPanel, textObject;
+    public GameObject chatPanel, textObject, chatLog;
     public InputField chatBox;
 
-    public Color playerMessage, info;
+    public Color playerMessage, info, errorColor;
 
     [SerializeField] List<Message> messageList = new List<Message>();
 
+    [HideInInspector] public List<Command> Commands = new List<Command>();
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space) && chatBox.text == "")
+        {
+            if (chatLog.activeSelf == false)
+                chatLog.SetActive(true);
+            else
+                chatLog.SetActive(false);
+        }
+
         if(chatBox.text != "")
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                SendMessageToChat(userName + ": " + chatBox.text, Message.MessageType.playerMessage);
-                chatBox.text = "";
+                if(chatBox.text[0] == '/')
+                {
+                    string[] arguments = chatBox.text.Split(' ');
+                    string commandName = arguments[0].Substring(1);
+
+                    Command command = Commands.Find(x => x.name == commandName);
+
+                    if(command == null)
+                    {
+                        SendMessageToChat(userName + ": ERROR - Command not found", Message.MessageType.error);
+                    }
+                    else
+                    {
+                        string[] parameters = new string[arguments.Length - 1];
+
+                        Array.Copy(arguments, 1, parameters, 0, parameters.Length);
+
+                        command.action.Invoke(parameters);
+
+                        SendMessageToChat(userName + ": " + commandName + " executed", Message.MessageType.info);
+                    }
+
+                    chatBox.text = "";
+                }
+                else
+                {
+                    SendMessageToChat(userName + ": " + chatBox.text, Message.MessageType.playerMessage);
+                    chatBox.text = "";
+                }
             }
         }
         else if (!chatBox.isFocused && Input.GetKeyDown(KeyCode.Return))
         {
             chatBox.ActivateInputField();
-        }
-
-        if (!chatBox.isFocused)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SendMessageToChat("You pressed space", Message.MessageType.info);
-                Debug.Log("Space");
-            }
         }
     }
 
@@ -69,12 +106,21 @@ public class GameManager : MonoBehaviour
 
         switch (messageType)
         {
+            case Message.MessageType.error:
+                color = errorColor;
+                break;
             case Message.MessageType.playerMessage:
                 color = playerMessage;
                 break;
         }
 
         return color;
+    }
+
+    public void AddCommand(string commandName, UnityAction<string[]> commandAction)
+    {
+        Command newCommand = new Command(commandName, commandAction);
+        Commands.Add(newCommand);
     }
 }
 
@@ -88,6 +134,20 @@ public class Message
     public enum MessageType
     {
         playerMessage,
-        info
+        info,
+        error
+    };
+}
+
+[System.Serializable]
+public class Command
+{
+    public string name;
+    public UnityAction<string[]> action;
+
+    public Command(string _name, UnityAction<string[]> _action)
+    {
+        name = _name;
+        action = _action;
     }
 }
