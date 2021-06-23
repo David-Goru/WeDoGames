43d3007bb203cf92a83dc1c64c072120192a8fe7
SergioPucela <44496290+SergioPucela@users.noreply.github.com>
2021-06-23 16:37:38 +0200

    Knockback state bug fixes

--- Tower Defense/Assets/Scripts/AI/Knockback.cs
+++ Tower Defense/Assets/Scripts/AI/Knockback.cs
@@ -10,6 +10,7 @@ public class Knockback : State
 	Vector3 pushDirection;
 	float maxViewRange = 2f;
 	float lerpSpeed = 5f;
+	Vector3 originPos;
 
 	public Knockback(Base_AI _npc, Animator _anim, Transform _target) : base(_npc, _anim, _target)
 	{
@@ -18,33 +19,39 @@ public class Knockback : State
 
 	public override void Enter()
 	{
+		anim.ResetTrigger("moving");
 		anim.SetTrigger("stunned");
 		base.Enter();
 
 		npc.CurrentTurret = null;
 		pushDistance = npc.PushDistance;
 		pushDirection = npc.PushDirection;
+		originPos = npc.transform.position;
 	}
 
 	public override void Update()
 	{
-        if (Vector3.Distance(npc.transform.position, pushDirection * pushDistance + npc.transform.position) < 0.1f)
+        if (Vector3.Distance(npc.transform.position, pushDirection * pushDistance + originPos) < 0.1f)
         {
-			npc.transform.position = -npc.transform.forward * pushDistance + npc.transform.position;
 			nextState = new Move(npc, anim, Target);
 			stage = EVENT.EXIT;
+			//Debug.Log("Push distance reached");
 		}
 		else if (Physics.Raycast(npc.transform.position, pushDirection, out hit, maxViewRange))
 		{
+			//Debug.Log("Raycast entered");
 			if (hit.transform.CompareTag("Turret"))
 			{
 				nextState = new Move(npc, anim, Target);
 				stage = EVENT.EXIT;
+				//Debug.Log("Push hit reached");
 			}
 		}
 		else
 		{
-			npc.transform.position = Vector3.Lerp(npc.transform.position, pushDirection * pushDistance + npc.transform.position, lerpSpeed * Time.deltaTime);
+			//pushDirection * pushDistance + npc.transform.position si NO quiero un movimiento que decelere al llegar al destino
+			npc.transform.position = Vector3.Lerp(npc.transform.position, pushDirection * pushDistance + originPos, lerpSpeed * Time.deltaTime);
+			//Debug.Log("Pushing...");
 		}
 	}
 
