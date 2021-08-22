06a7b2a0865ad14d7e4b420ad54cd6bb4bebc128
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-22 13:26:48 +0200

    Added snap rotation

--- Tower Defense/Assets/Scripts/General/PlayerCamera.cs
+++ Tower Defense/Assets/Scripts/General/PlayerCamera.cs
@@ -1,11 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerCamera : MonoBehaviour
 {
     float sensitivity = 5.0f;
+    bool isRotating = false;
 
     void Update()
     {
-        if (Input.GetMouseButton(2)) transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * sensitivity, 0), Space.World);
+        if (Input.GetMouseButton(2) && Mathf.Abs(Input.GetAxis("Mouse X")) > 0.2f) rotate();
+    }
+
+    void rotate()
+    {
+        if (isRotating) return;
+
+        int direction = Input.GetAxis("Mouse X") > 0 ? 1 : -1;
+        StartCoroutine(snapRotation(direction));
+    }
+
+    IEnumerator snapRotation(int direction)
+    {
+        isRotating = true;
+
+        int degreesLeft = 90;
+        while (degreesLeft > 0)
+        {
+            degreesLeft--;
+            int yRotation = Mathf.RoundToInt(transform.rotation.eulerAngles.y + direction);
+            transform.eulerAngles = new Vector3(30, yRotation, 0);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        yield return new WaitForSeconds(0.1f);
+        isRotating = false;
     }
 }
