2ab9cb55ec4a0601e09c41aee11c6e538bac4fd1
David Gómez <david.gomez.rooiz@gmail.com>
2021-08-23 18:57:58 +0200

    Change AI material tweaks

--- Tower Defense/Assets/Scripts/AI/Base_AI.cs
+++ Tower Defense/Assets/Scripts/AI/Base_AI.cs
@@ -10,6 +10,7 @@ using UnityEngine.SceneManagement;
 public class Base_AI : Entity, ITurretDamage, IPooledObject, IStunnable, ISlowable, IFearable, IDamageable, IPoisonable, IKnockbackable, IDamageReductible
 {
     [SerializeField] EnemyInfo info = null;
+    [SerializeField] Color poisonColor = Color.green;
 
     [Header("Debug")]
     [SerializeField] Transform goal;
@@ -19,6 +20,7 @@ public class Base_AI : Entity, ITurretDamage, IPooledObject, IStunnable, ISlowab
 
     Animator anim;
     State currentState;
+    Material material;
 
     Vector3[] path;
     Vector3 currentWaypoint;
@@ -54,6 +56,7 @@ public class Base_AI : Entity, ITurretDamage, IPooledObject, IStunnable, ISlowab
 
     public void OnObjectSpawn()
     {
+        setUpMaterial();
         title = info.name;
         currentHP = Info.MaxHealth;
         maxHP = Info.MaxHealth;
@@ -80,6 +83,26 @@ public class Base_AI : Entity, ITurretDamage, IPooledObject, IStunnable, ISlowab
     void OnDisable()
     {
         StopCoroutine("FollowPath");
+    }
+
+    void setUpMaterial()
+    {
+        Transform model = transform.Find("Model");
+        if (model != null)
+        {
+            Transform modelWithColor = model.Find("Color");
+            if (modelWithColor != null)
+            {
+                Renderer renderer = model.Find("Color").GetComponent<Renderer>();
+                material = renderer.material;
+            }
+        }
+    }
+
+    void changeMaterialColor(Color newColor)
+    {
+        if (material == null) return;
+        material.color = newColor;
     }
 
     bool isTargetTurretDead()
@@ -240,30 +263,24 @@ public class Base_AI : Entity, ITurretDamage, IPooledObject, IStunnable, ISlowab
         if (currentPoison != null)
         {
             StopCoroutine(currentPoison);
-            transform.Find("Model").Find("Color").GetComponent<Renderer>().material.color = new Color(1, 1, 1);
+            changeMaterialColor(Color.white);
         }
         currentPoison = StartCoroutine(poisonEnemy(secondsPoisoned, damagePerSecond));
     }
 
     IEnumerator poisonEnemy(float secondsPoisoned, float damagePerSecond)
     {
-        // Enable visual effects?
-        Material material = transform.Find("Model").Find("Color").GetComponent<Renderer>().material;
-        Color defaultColor = material.color;
-        Color poisonColor = new Color(1, 0, 0);
+        changeMaterialColor(poisonColor);
 
         int timer = 0;
-        material.color = poisonColor;
         while (timer < secondsPoisoned)
         {
             yield return new WaitForSeconds(1f);
             GetDamage(Mathf.RoundToInt(damagePerSecond));
             timer++;
         }
-        material.color = defaultColor;
 
-        // Disable visual effects?
-
+        changeMaterialColor(Color.white);
         currentPoison = null;
     }
 
