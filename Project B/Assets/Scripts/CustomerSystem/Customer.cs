0ac118a0346b54407fd1566d06fc20802485ca95
arturo101199 <al375559@uji.es>
2021-11-02 14:08:41 +0100

    Customer basic asking for products system

--- Project B/Assets/Scripts/CustomerSystem/Customer.cs
+++ Project B/Assets/Scripts/CustomerSystem/Customer.cs
@@ -3,9 +3,15 @@ using UnityEngine.AI;
 
 public class Customer : MonoBehaviour, IPooledObject
 {
+    [SerializeField] string[] productsToAsk;
+    [SerializeField] float waitingTime = 15f;
     CustomerWaitingPlace[] customerWaitingPlaces;
+    CustomerWaitingPlace currentCustomerWaitingPlace;
     NavMeshAgent agent;
+    bool isGoingIntoAWaitingPlace = false;
     bool isInWaitingPlace = false;
+
+    float timer = 0f;
 
     private void Awake()
     {
@@ -27,18 +33,46 @@ public class Customer : MonoBehaviour, IPooledObject
 
     private void Update()
     {
-        if (!isInWaitingPlace) tryToGoToAWaitingPlace();
+        if (!isGoingIntoAWaitingPlace && !isInWaitingPlace) tryToGoToAWaitingPlace();
+        else if(isGoingIntoAWaitingPlace)
+        {
+            if(agent.remainingDistance <= 0.1f)
+            {
+                isInWaitingPlace = true;
+                isGoingIntoAWaitingPlace = false;
+                askForSomething();
+            }
+        }
+        else
+        {
+            if (timer >= waitingTime)
+            {
+                ObjectPooler.GetInstance().ReturnToThePool(transform);
+                currentCustomerWaitingPlace.IsOccupied = false;
+            }
+            else timer += Time.deltaTime;
+        }
+    }
+
+    private void askForSomething()
+    {
+        int randomNumber = Random.Range(0, productsToAsk.Length);
+        print("Please give me a " + productsToAsk[randomNumber]);
     }
 
     void goToWaitingPlace(CustomerWaitingPlace place)
     {
         place.IsOccupied = true;
-        isInWaitingPlace = true;
+        currentCustomerWaitingPlace = place;
+        isGoingIntoAWaitingPlace = true;
         agent.SetDestination(place.transform.position);
     }
 
     public void OnObjectSpawn()
     {
+        timer = 0f;
+        isGoingIntoAWaitingPlace = false;
+        isInWaitingPlace = false;
         tryToGoToAWaitingPlace();
     }
 }
